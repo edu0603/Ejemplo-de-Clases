ce616c9a90d3e9f6507a4a7474935b14ce65f270
Alfredo Vanegas <alfredo@pop-os.localdomain>
2021-03-11 17:15:16 -0600

    Ejemplo

--- src/Marinos/Pez.java
+++ src/Marinos/Pez.java
@@ -4,4 +4,8 @@ public class Pez extends Marinos {
     Pez(String sistemaDigestivo, String tiempoDeVida, boolean tieneAletas, Double temperatura, Double presión) {
         super(sistemaDigestivo, tiempoDeVida, tieneAletas, temperatura, presión);
     }
+
+    public static void main(String[] args) {
+       Pez Pez = new Pez("Herbivoro", "10 años", true, 70.0, 70.0);
+    }
 }
