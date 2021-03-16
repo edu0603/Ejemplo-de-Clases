dcd873e6d68bdc682ae722c742404eb6b26147e4
ada <ada@kali.domain>
2021-03-16 11:13:23 -0600

    Boilerplate Constructor para estos archivos

---
+++ src/Marinos/Delfín.java
@@ -0,0 +1,7 @@
+package Marinos;
+
+public class Delfín extends Marinos {
+    Delfín(String sistemaDigestivo, String tiempoDeVida, boolean tieneAletas, Double temperatura, Double presión) {
+        super(sistemaDigestivo, tiempoDeVida, tieneAletas, temperatura, presión);
+    }
+}
---
+++ src/Marinos/Tiburón.java
@@ -0,0 +1,7 @@
+package Marinos;
+
+public class Tiburón extends Marinos{
+    Tiburón(String sistemaDigestivo, String tiempoDeVida, boolean tieneAletas, Double temperatura, Double presión) {
+        super(sistemaDigestivo, tiempoDeVida, tieneAletas, temperatura, presión);
+    }
+}
