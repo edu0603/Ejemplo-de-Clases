98ffe2fd4a0818d71c1f47d1b98e499b8c43fdcf
itsyaelx <ruizyael54@gmail.com>
2021-03-16 18:54:09 -0600

    Propuesta: Clase Terrestre

--- src/Terrestres/Terrestres.java
+++ src/Terrestres/Terrestres.java
@@ -8,14 +8,38 @@ class Terrestres extends Animales {
     protected boolean tienePatas;
 
     public Terrestres(String sistemaDigestivo, String tiempoDeVida,String tipoDeRespiracion,boolean tienePatas){
-	super(sistemaDigestivo, tiempoDeVida);
+	super(String sistemaDigestivo, String tiempoDeVida);
+	
+	this.tipoDeRespiracion = tipoDeRespiracion;
+	this.tienePatas = tienePatas;
+	
     }
-
-    public void reproducirse(){
-
+    
+    public void setTipoDeRespiracion (String tipoDeRespiracion){
+    	this.tipoDeRespiracion = tipoDeRespiracion;
     }
-
-    public void respirar(){
-
+    
+	    public String getTipoDeRespiracion () {
+	    	return tipoDeRespiracion;
+	    }
+    
+    public void setTienePatas (boolean tienePatas){
+    	this.tienePatas = tienePatas;
+    }
+    
+    public boolean getTienePatas () {
+    	return tienePatas;
+    }
+    
+    public void caminar() {
+    	
+    	if (tienePatas == true) {
+    		System.out.println("Esta animal tiene la capacidad de caminar.");
+    	}
+    	
+    	else {
+    		System.out.println("Este animal no posee la capacidad de caminar");
+    	}
+    	
     }
 }
